3580a48109c7c2ee04cb4d0b6d845ef0e873fb68
knwpound <pound.somparn@gmail.com>
2026-04-11 16:57:14 +0700

    add: dev & ux data

---
+++ src/lib/dev.tsx
@@ -0,0 +1,141 @@
+export const DEV_SKILLS_DATA = [
+  {
+    title: "Agile Experience",
+    desc: "Worked in Agile teams through projects and internship with iterative delivery",
+    rotate: "-rotate-3",
+  },
+  {
+    title: "Frontend",
+    desc: "Build responsive UI with Next.js, focusing on performance and UX",
+    rotate: "rotate-2",
+  },
+  {
+    title: "UI/UX Design",
+    desc: "Wireframing, design systems, and interactive prototyping in Figma",
+    rotate: "-rotate-1",
+  },
+  {
+    title: "Backend",
+    desc: "Design APIs, manage data flow, and integrate MongoDB & MySQL",
+    rotate: "rotate-3",
+  },
+  {
+    title: "Infrastructure",
+    desc: "Basic cloud and deployment using Docker, AWS, and system architecture",
+    rotate: "-rotate-2",
+  },
+  {
+    title: "Languages",
+    desc: "C++, Python, Go, TypeScript, Java, and C for various applications",
+    rotate: "rotate-1",
+  },
+];
+
+export const DEV_PROJECTS = [
+  {
+    title: "CP Website",
+    category: "Capstone",
+    year: "Senior year",
+    image: "/pics/capstone.png",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "UX", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description:
+      "This project is about redesigning a 10-year-old website into a modern ,user-friendly ,and responsive interface. It was my first project working with real users, where I took part in requirement gathering and UI Lead",
+    link: "https://cp.ratchanon.works/",
+    linkLabel: "Link",
+  },
+  {
+    title: "Event Management",
+    category: "Sw Dev Prac II",
+    year: "Senior year",
+    image: "/pics/swdev.png",
+    imageMode: "object-cover",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description:
+      "My solo project for Event Management web application. Simple design for product management website with user and admin view.",
+    link: "https://swdevprac2-term-project.vercel.app/",
+    linkLabel: "Link",
+  },
+  {
+    title: "Realtime Chat App",
+    category: "Network",
+    year: "Junior year",
+    image: "/pics/chat.png",
+    imageMode: "object-cover",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+      { label: "PM", color: "bg-fuchsia-200" },
+    ],
+    description:
+      "Real-time chat application built with Socket.io. I took the lead on this project, handling planning, task allocation, designer and developer. The design concept focuses on simplicity, clarity, and familiarity for users.",
+    link: "https://www.youtube.com/watch?si=5sxzXQzCcKPKmXga&v=Hhcnxjao8YQ&feature=youtu.be",
+    linkLabel: "Figma",
+  },
+  {
+    title: "Auc Auc",
+    category: "SE2",
+    year: "Junior year",
+    image: "/pics/aucauc.png",
+    imageMode: "object-cover object-left",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description:
+      "This was my first agile web-based project, where I took on the roles of UX/UI designer and frontend developer. It was also my first time gaining hands-on experience with Figma.",
+    link: "https://github.com/KanisornPutt/software-eng-2-client",
+    linkLabel: "Github",
+  },
+  {
+    title: "JavaFx Game Project",
+    category: "Progmeth",
+    year: "Sophomore year",
+    video: "https://www.youtube.com/embed/s6bN0dfBuLM?mute=1", // เพิ่มฟิลด์ video สำหรับเคส iframe
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+      { label: "PM", color: "bg-fuchsia-200" },
+    ],
+    description:
+      "This was my very first project ! Moo Day is a lively farming game built with Java and JavaFX, under the OOP concept. I took the lead on planning and developing from start to finish.",
+    link: "https://youtu.be/s6bN0dfBuLM",
+    linkLabel: "Link",
+  },
+  {
+    title: "Box & Bone",
+    category: "ComEngEss",
+    year: "Sophomore year",
+    image: "/pics/game.png",
+    imageMode: "object-cover",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+      { label: "PM", color: "bg-fuchsia-200" },
+    ],
+    description:
+      "Box & Bone is a fun game similar to Pac-Man, where players race to collect as many coins as possible within a set time limit! I have taken on the roles of task manager and full-stack developer for this exciting project!",
+    link: "#",
+    linkLabel: "More",
+  },
+  {
+    title: "Water Sensor",
+    category: "Embedded",
+    year: "Sophomore year",
+    videoFile: "/vdo/smart.mp4",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description:
+      "This project is about a water level sensor used in a dam. I designed the entire website's UI/UX. To make it feel more realistic, I added animated water waves, and the container can be scaled to match real-life sizes.",
+    link: "https://smart-floodgate.web.app",
+    linkLabel: "Link",
+  },
+];
---
+++ src/lib/uxui.tsx
@@ -0,0 +1,112 @@
+export const UX_SKILLS_DATA = [
+  {
+    title: "Research",
+    desc: "User Interview, Related-product Auditing, User Persona & User Journey",
+    rotate: "-rotate-3",
+  },
+  {
+    title: "UI Skills",
+    desc: "Wireframing (Low & High Fidelity), Design Systems, Basic Design Knowledge",
+    rotate: "rotate-2",
+  },
+  {
+    title: "Figma",
+    desc: "Design System & Token, Interactive Prototyping: Auto Layout, Components",
+    rotate: "-rotate-1",
+  },
+  {
+    title: "Testing",
+    desc: "Iterative design process including User Testing with Feedback",
+    rotate: "rotate-3",
+  },
+  {
+    title: "Coding",
+    desc: "Intermediate Software Developer with internship experience",
+    rotate: "-rotate-2",
+  },
+];
+
+export const UX_PROJECTS = [
+  {
+    title: "CP Website",
+    category: "Capstone",
+    year: "Senior year",
+    image: "/pics/capstone.png",
+    tags: [
+      { label: "UX", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description: "This project is about redesigning a 10-year-old website into a modern ,user-friendly ,and responsive interface. It was my first project working with real users, where I took part in requirement gathering and UI Lead",
+    link: "https://cp.ratchanon.works/",
+    linkLabel: "Link"
+  },
+  {
+    title: "Plookpanya School",
+    category: "Side Project",
+    year: "Senior year",
+    image: "/pics/ppy.png",
+    tags: [
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description: "This was my first side project focusing on redesigning an outdated website into a modern and academic interface. I aimed to create adesign that felt unique and with distinctive interactions and elements that stood out.",
+    link: "https://www.figma.com/design/Y2eAMZ9AV4BRHSSOB1o0JI/PPY-Website-Design?node-id=413-4526&t=fECyz2HAs1UaWzOM-1",
+    linkLabel: "Figma"
+  },
+  {
+    title: "Event Management",
+    category: "Sw Dev Prac II",
+    year: "Senior year",
+    image: "/pics/swdev.png",
+    imageMode: "object-cover",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description: "My solo project for Event Management web application. Simple design for product management website with user and admin view.",
+    link: "https://swdevprac2-term-project.vercel.app/",
+    linkLabel: "Link"
+  },
+  {
+    title: "Fitness App",
+    category: "UxUi",
+    year: "Senior year",
+    image: "/pics/fitness.png",
+    imageMode: "object-contain",
+    tags: [
+      { label: "UX", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description: "Another UX/UI project focused on creating a mobile application for non-trainer fitness users. I worked on understanding the user persona, refining the design through iteration, and building a structured design system.",
+    link: "https://www.figma.com/design/LrfaZHiF1lie1aAPwezPzn/Fitness-App?node-id=421-3582&t=IFnhHE5eiWnnPP7r-1",
+    linkLabel: "Figma"
+  },
+  {
+    title: "Realtime Chat App",
+    category: "Network",
+    year: "Junior year",
+    image: "/pics/chat.png",
+    imageMode: "object-cover",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+      { label: "PM", color: "bg-fuchsia-200" },
+    ],
+    description: "Real-time chat application built with Socket.io. I took the lead on this project, handling planning, task allocation, designer and developer. The design concept focuses on simplicity, clarity, and familiarity for users.",
+    link: "https://www.figma.com/design/r3L2Ix6JiQNoDwvwX9YbH9/Network?node-id=0-1&t=cFl1NPIpO3S8wHQZ-1",
+    linkLabel: "Figma"
+  },
+  {
+    title: "Auc Auc",
+    category: "SE2",
+    year: "Junior year",
+    image: "/pics/aucauc.png",
+    imageMode: "object-cover object-left",
+    tags: [
+      { label: "Dev", color: "bg-lime-400/50" },
+      { label: "Designer", color: "bg-violet-300/80" },
+    ],
+    description: "This was my first agile web-based project, where I took on the roles of UX/UI designer and frontend developer. It was also my first time gaining hands-on experience with Figma.",
+    link: "https://www.figma.com/design/NOZXtEpBdPDw014iA7XWfZ/aucauc?node-id=1-2&t=rC3FDK6EALIX1Men-1",
+    linkLabel: "Figma"
+  }
+];
