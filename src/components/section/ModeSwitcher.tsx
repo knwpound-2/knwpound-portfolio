9cb3884f960fae930f210c0744cb9b752b11cdc8
knwpound <pound.somparn@gmail.com>
2026-04-11 16:57:58 +0700

    Create ModeSwitcher.tsx

---
+++ src/components/section/ModeSwitcher.tsx
@@ -0,0 +1,11 @@
+import ModeButton from "../ui/ModeButton";
+
+export default function ModeSwitcher() {
+  return (
+    <div className="flex gap-2 fixed top-2 right-[10px] z-50">
+      <ModeButton 
+      />
+
+    </div>
+  );
+}
