d448ed3f526f3986c5834392343ada3bc0ea3c86
knwpound <pound.somparn@gmail.com>
2026-04-11 16:57:33 +0700

    Update FigmaEmbed.tsx

--- src/components/ui/FigmaEmbed.tsx
+++ src/components/ui/FigmaEmbed.tsx
@@ -1,11 +1,11 @@
 // components/FigmaEmbed.tsx
 
-export default function FigmaEmbed({ figmaLink }: { figmaLink: string }) {
+export default function LinkEmbed({ link }: { link: string }) {
   return (
     <div className="w-full h-[450px] lg:h-[600px]"> 
       <iframe
         className="w-full h-full rounded-xl border border-black/10 shadow-sm"
-        src={figmaLink}
+        src={link}
         allowFullScreen
         title="Figma Prototype"
       />
